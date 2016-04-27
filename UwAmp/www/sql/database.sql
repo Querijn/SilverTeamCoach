db7b3dc62e5b6db19a7592c300deb2a4ea48b8e8
Querijn Heijmans <querijn@irule.at>
2016-04-27 09:45:26 +0200

    Forgot small database change
    - Added new git ignore for key file
    - Added database changes

--- UwAmp/www/sql/database.sql
+++ UwAmp/www/sql/database.sql
@@ -1,18 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Gegenereerd op: 27 apr 2016 om 07:42
+-- Serverversie: 5.7.11
+-- PHP-versie: 5.6.18
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `stc`
+--
 CREATE DATABASE IF NOT EXISTS `stc` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `stc`;
 
-CREATE TABLE `PREFIXGOESHEREplayers` (
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `players`
+--
+
+CREATE TABLE `players` (
   `id` int(11) NOT NULL,
   `user` int(11) NOT NULL,
+  `title` enum('Player','Challenger','Developer','') NOT NULL,
+  `cash` double NOT NULL,
   `admin` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Indexen voor geëxporteerde tabellen
+--
 
-ALTER TABLE `PREFIXGOESHEREplayers`
+--
+-- Indexen voor tabel `players`
+--
+ALTER TABLE `players`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
 
+--
+-- AUTO_INCREMENT voor een tabel `players`
+--
 ALTER TABLE `players`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
