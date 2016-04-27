7654175d3e4ef804458ef939eaa3264e78a636d2
Liane <Liane.gaasendam@gmail.com>
2016-04-27 13:53:55 +0200

    UI shop thingies
    - sort lowest/higest
    - red color unbuyable
    - champions appear (without image)

---
+++ Assets/Scripts/ShopManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShopManager : MonoBehaviour
+{
+    bool Done = false;
+    public void SetupShop(Champion[] ChampionArray = null)
+    {
+        if(ChampionArray == null)
+        {
+            ChampionArray = Champion.All;
+        }
+
+        GameObject Prefab = Resources.Load("Prefabs/Champion") as GameObject;
+        GameObject ShopContent = GameObject.FindGameObjectWithTag("ShopContent");
+
+        int I = 0;
+        int J = 0;
+        
+        foreach (Transform Child in ShopContent.transform)
+            Destroy(Child.gameObject);
+
+        foreach (Champion Champion in ChampionArray)
+        {
+            GameObject Instance = Instantiate(Prefab) as GameObject;
+            Instance.transform.SetParent(ShopContent.transform);
+            Instance.transform.localPosition = new Vector3((I * 250) + 2, (J * 300), 0);
+
+
+            Instance.transform.Find("Name").GetComponent<Text>().text = Champion.Name;
+            Instance.transform.Find("Price").GetComponent<Text>().text = Cash.Format(Champion.Price);
+
+            //string Image = "http://ddragon.leagueoflegends.com/cdn/6.8.1/img/sprite/champion1.png";
+            //break;
+            //HTTP.Request(Image, delegate (WWW Request)
+            //{
+            //    Instance.transform.Find("Image").GetComponent<SpriteRenderer>().sprite = Sprite.Create(Request.texture, new Rect(0, 0, Request.texture.width, Request.texture.height), Vector2.zero);
+            //});
+    
+            I += 1;
+            if (I>4)
+            {
+                J = J - 1;
+                I = 0;
+            }
+            if(Champion.Price > Info.Player.Cash)
+            {
+                Instance.transform.Find("Price").GetComponent<Text>().color = Color.red;
+            }
+        }
+    }
+    
+    void Update ()
+    {
+	    if(Champion.All.Length != 0 && Done == false)
+        {
+            SetupShop();
+            Done = true;
+        }
+	}
+}
+
+//sorteren op laagste/hoogste prijs
---
+++ Assets/Scripts/SortButton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SortButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+   public void OnClick()
+    {
+        Champion[] ChampionArray = Champion.All;
+        Array.Sort(ChampionArray, delegate (Champion Champ1, Champion Champ2)
+        {
+            return Champ1.Price.CompareTo(Champ2.Price);
+        });
+
+        ShopManager Shop = GameObject.FindGameObjectWithTag("Shop").GetComponent<ShopManager>();
+        Shop.SetupShop(ChampionArray);
+        Debug.Log(ChampionArray[0].Price);
+    }
+    
+}
