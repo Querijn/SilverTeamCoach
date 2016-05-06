0be69bd2ac5fd294e195e52d44d7907783fa52a7
Querijn Heijmans <querijn@irule.at>
2016-05-06 12:56:54 +0200

    Started on matchmaking in Unity
    - Removed sort button
    - Allowed team load at all times

---
+++ Assets/Scripts/Matchmaking.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Matchmaking : MonoBehaviour 
+{
+    static bool m_Setup = false;
+    public static void Reset()
+    {
+        m_Setup = false;
+    }
+
+    Dropdown m_TeamSelector = null;
+
+	void Start ()
+	{
+        m_TeamSelector = GetComponentInChildren<Dropdown>();
+	}
+	
+	void Update () 
+	{
+
+	    if(m_Setup == false && Team.All.Length != 0)
+        {
+            Debug.Log("Teams are here!");
+            m_Setup = true;
+        }
+	}
+}
--- Assets/Scripts/TeamManager.cs
+++ Assets/Scripts/TeamManager.cs
@@ -12,10 +12,12 @@ public class TeamManager : MonoBehaviour
 
     static bool m_Setup = false;
     static bool m_InProgress = false;
+    static JSONArray m_Array = null;
     public static void Reset()
     {
         m_Setup = false;
         m_InProgress = false;
+        m_Array = null;
     }
 
 	void Update () 
@@ -26,44 +28,49 @@ public class TeamManager : MonoBehaviour
             {
                 var t_JSON = JSON.Parse(a_Request.text);
 
-                GameObject t_Prefab = Resources.Load("Prefabs/TeamListObject") as GameObject;
-                RectTransform t_PrefabTransform = t_Prefab.GetComponent<RectTransform>();
-                GameObject t_Content = GameObject.FindGameObjectWithTag("TeamsContent");
+                m_Array = t_JSON.AsArray;
 
-                foreach (Transform t_Child in t_Content.transform)
-                    Destroy(t_Child.gameObject);
-
-                JSONArray t_Array = t_JSON.AsArray;
-
-                Team.Setup(t_Array);
-
-                int y = 0;
-                foreach (JSONNode t_Team in t_Array)
-                {
-                    GameObject t_Instance = Instantiate(t_Prefab);
-                    t_Instance.transform.SetParent(t_Content.transform);
-
-                    t_Instance.name = t_Team["name"];
-                    t_Instance.transform.Find("Name").GetComponent<Text>().text = t_Team["name"].Value;
-                    t_Instance.transform.Find("ID").GetComponent<Text>().text = t_Team["id"].Value;
-
-                    t_Instance.transform.Find("Champions/Top").GetComponentInChildren<Image>().sprite = Champion.Get(t_Team["top"].AsInt).Image;
-                    t_Instance.transform.Find("Champions/Mid").GetComponentInChildren<Image>().sprite = Champion.Get(t_Team["mid"].AsInt).Image;
-                    t_Instance.transform.Find("Champions/Support").GetComponentInChildren<Image>().sprite = Champion.Get(t_Team["support"].AsInt).Image;
-                    t_Instance.transform.Find("Champions/Marksman").GetComponentInChildren<Image>().sprite = Champion.Get(t_Team["marksman"].AsInt).Image;
-                    t_Instance.transform.Find("Champions/Jungle").GetComponentInChildren<Image>().sprite = Champion.Get(t_Team["jungle"].AsInt).Image;
-
-                    t_Instance.transform.localPosition = new Vector3(0, (-y * t_PrefabTransform.sizeDelta.y));
-                    t_Instance.transform.localScale = Vector3.one;
-                    y++;
-                }
-
-                t_Content.GetComponent<RectTransform>().sizeDelta = new Vector2(0, 40 + (y * t_PrefabTransform.sizeDelta.y));
-
+                Team.Setup(m_Array);
+                
                 //t_JSON
                 m_Setup = true;
             }, true);
             m_InProgress = true;
+        }
+
+        if(m_Array != null && GameObject.FindGameObjectWithTag("TeamsContent") != null)
+        {
+            GameObject t_Content = GameObject.FindGameObjectWithTag("TeamsContent");
+
+            foreach (Transform t_Child in t_Content.transform)
+                Destroy(t_Child.gameObject);
+
+            GameObject t_Prefab = Resources.Load("Prefabs/TeamListObject") as GameObject;
+            RectTransform t_PrefabTransform = t_Prefab.GetComponent<RectTransform>();
+
+            int y = 0;
+            foreach (JSONNode t_Team in m_Array)
+            {
+                GameObject t_Instance = Instantiate(t_Prefab);
+                t_Instance.transform.SetParent(t_Content.transform);
+
+                t_Instance.name = t_Team["name"];
+                t_Instance.transform.Find("Name").GetComponent<Text>().text = t_Team["name"].Value;
+                t_Instance.transform.Find("ID").GetComponent<Text>().text = t_Team["id"].Value;
+
+                t_Instance.transform.Find("Champions/Top").GetComponentInChildren<Image>().sprite = Champion.Get(t_Team["top"].AsInt).Image;
+                t_Instance.transform.Find("Champions/Mid").GetComponentInChildren<Image>().sprite = Champion.Get(t_Team["mid"].AsInt).Image;
+                t_Instance.transform.Find("Champions/Support").GetComponentInChildren<Image>().sprite = Champion.Get(t_Team["support"].AsInt).Image;
+                t_Instance.transform.Find("Champions/Marksman").GetComponentInChildren<Image>().sprite = Champion.Get(t_Team["marksman"].AsInt).Image;
+                t_Instance.transform.Find("Champions/Jungle").GetComponentInChildren<Image>().sprite = Champion.Get(t_Team["jungle"].AsInt).Image;
+
+                t_Instance.transform.localPosition = new Vector3(0, (-y * t_PrefabTransform.sizeDelta.y));
+                t_Instance.transform.localScale = Vector3.one;
+                y++;
+            }
+
+            t_Content.GetComponent<RectTransform>().sizeDelta = new Vector2(0, 40 + (y * t_PrefabTransform.sizeDelta.y));
+            m_Array = null;
         }
 	}
 
