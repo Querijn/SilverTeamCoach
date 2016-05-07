8eb61cd603c952b9e470c23003954c87afaf8a2b
Liane <Liane.gaasendam@gmail.com>
2016-05-07 14:03:19 +0200

    Added Settings page
    - Music volume slider
    - Sound effects slider

---
+++ Assets/SettingsModifier.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SettingsModifier : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+    void OnSlideMusicVolume()
+    {
+
+    }
+
+    void OnSlideSEVolume ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
